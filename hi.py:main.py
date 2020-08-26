f439f23691260ff479ca916f22bed5ab75a2b6a8
Tymur-Salo <64163592+Tymur-salo@users.noreply.github.com>
2020-08-26 12:34:09 +0200

    Update hi.py:main.py

--- hi.py:main.py
+++ hi.py:main.py
@@ -23,5 +23,11 @@ def say_is_int32(value):
 		say("Is not in32!")
 	else:
 		say("Is in32!")
-
-
+def _Me(x: int):
+	return [x for i in range(x if not 0 else 10)]
+def _standart_string_value(rang: int = 9):
+	i=[i for i in range(rang if not 0 else 10)]
+	__str=""
+	for j in range(len(i)):
+		__str+=str(i[j])
+	return __str
